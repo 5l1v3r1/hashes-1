10a7dc459095a2fd88f60277527b5a57382b85b6
Leon V <33411715+leonv024@users.noreply.github.com>
2018-09-18 11:35:00 +0200

    Added checkboxes and file dialog
    This makes hashing big files (such as a ISO) a lot faster

--- hashes.py
+++ hashes.py
@@ -2,14 +2,15 @@
 import os, sys, platform, getpass, hashlib
 from tkinter import *
 from tkinter.ttk import *
+from tkinter.filedialog import askopenfilename # Import filedialog
 
 class MainWindow(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.title(string = "Hashes") # Set window title
         self.resizable(0,0) # Do not allow to be resized
-        self.style = Style()
-        self.style.theme_use("clam")
+        self.style = Style() # Enable themes
+        self.style.theme_use("clam") # Theme name
         icon = PhotoImage(file='icon.png') # Set app icon
         self.tk.call('wm', 'iconphoto', self._w, icon) # Call app icon
 
@@ -22,6 +23,12 @@ class MainWindow(Tk):
             'SHA256' : StringVar(),
             'SHA384' : StringVar(),
             'SHA512' : StringVar(),
+
+            'en_md5' : IntVar(),
+            'en_sha1' : IntVar(),
+            'en_sha256' : IntVar(),
+            'en_sha384' : IntVar(),
+            'en_sha512' : IntVar(),
         }
 
         # Set default values
@@ -32,6 +39,9 @@ class MainWindow(Tk):
         else:
             self.options['file'].set('C:\\Users\\' + getpass.getuser() + '\\Desktop\\' )
 
+        self.options['en_sha1'].set(1)
+        self.options['en_sha256'].set(1)
+
         # Label Frame
         settings = LabelFrame(self, text = 'Data', relief = GROOVE, labelanchor = 'nw', width = 440, height = 100)
         settings.grid(row = 0, column = 1)
@@ -40,6 +50,7 @@ class MainWindow(Tk):
         # File entry
         Label(settings, text = 'File:').grid(row = 0, column = 0)
         Entry(settings, textvariable = self.options['file'], width = 45).grid(row = 0, column = 1, columnspan = 2)
+        select_file = Button(settings, text = '...', command = self.file_browser, width = 2).grid(row = 0, column = 3)
 
         # Hash entry
         Label(settings, text = 'Hash:').grid(row = 1, column = 0)
@@ -51,19 +62,24 @@ class MainWindow(Tk):
         encodings.grid_propagate(0)
 
         # Hashes entry
-        Label(encodings, text = 'MD5:').grid(row = 0, column = 0)
+        #Label(encodings, text = 'MD5:').grid(row = 0, column = 0)
+        Checkbutton(encodings, text="MD5", variable=self.options['en_md5']).grid(row = 0, column = 0)
         Entry(encodings, textvariable = self.options['MD5'], width = 45).grid(row = 0, column = 1, columnspan = 2)
 
-        Label(encodings, text = 'SHA1:').grid(row = 1, column = 0)
+        #Label(encodings, text = 'SHA1:').grid(row = 1, column = 0)
+        Checkbutton(encodings, text="SHA1", variable=self.options['en_sha1']).grid(row = 1, column = 0)
         Entry(encodings, textvariable = self.options['SHA1'], width = 45).grid(row = 1, column = 1, columnspan = 2)
 
-        Label(encodings, text = 'SHA256:').grid(row = 2, column = 0)
+        #Label(encodings, text = 'SHA256:').grid(row = 2, column = 0)
+        Checkbutton(encodings, text="SHA256", variable=self.options['en_sha256']).grid(row = 2, column = 0)
         Entry(encodings, textvariable = self.options['SHA256'], width = 45).grid(row = 2, column = 1, columnspan = 2)
 
-        Label(encodings, text = 'SHA384:').grid(row = 3, column = 0)
+        #Label(encodings, text = 'SHA384:').grid(row = 3, column = 0)
+        Checkbutton(encodings, text="SHA384", variable=self.options['en_sha384']).grid(row = 3, column = 0)
         Entry(encodings, textvariable = self.options['SHA384'], width = 45).grid(row = 3, column = 1, columnspan = 2)
 
-        Label(encodings, text = 'SHA512:').grid(row = 4, column = 0)
+        #Label(encodings, text = 'SHA512:').grid(row = 4, column = 0)
+        Checkbutton(encodings, text="SHA512", variable=self.options['en_sha512']).grid(row = 4, column = 0)
         Entry(encodings, textvariable = self.options['SHA512'], width = 45).grid(row = 4, column = 1, columnspan = 2)
 
         # Buttom Buttons frame
@@ -74,31 +90,38 @@ class MainWindow(Tk):
         # Bottom buttons
         show_hashes = Button(buttons, text = "HASH", command = self.hash_all, width = 53).grid(row = 0, column = 0, columnspan = 3)
         exit_app = Button(buttons, text = "EXIT", command = exit, width = 53).grid(row = 1, column = 0, columnspan = 3)
-        
-    def decode_base_now(self, event):
-        print('Comming Soon!')
-        self.enter_val.destroy()
+
+    def file_browser(self):
+        self.options['file'].set(askopenfilename())
 
     def hash_all(self):
-        hashes = {'md5': self.md5(),
-            'sha1': self.sha1(),
-            'sha256': self.sha256(),
-            'sha384': self.sha384(),
-            'sha512': self.sha512(),
-            }
 
-        self.options['MD5'].set(self.md5())
-        self.options['SHA1'].set(self.sha1())
-        self.options['SHA256'].set(self.sha256())
-        self.options['SHA384'].set(self.sha384())
-        self.options['SHA512'].set(self.sha512())
+        if self.options['en_md5'].get() == 1:
+            self.options['MD5'].set(self.md5())
+
+        if self.options['en_sha1'].get() == 1:
+            self.options['SHA1'].set(self.sha1())
+
+        if self.options['en_sha256'].get() == 1:
+            self.options['SHA256'].set(self.sha256())
+
+        if self.options['en_sha384'].get() == 1:
+            self.options['SHA384'].set(self.sha384())
+
+        if self.options['en_sha512'].get() == 1:
+            self.options['SHA512'].set(self.sha512())
+
+        hashes = {'md5': self.options['MD5'].get(),
+            'sha1': self.options['SHA1'].get(),
+            'sha256': self.options['SHA256'].get(),
+            'sha384': self.options['SHA384'].get(),
+            'sha512': self.options['SHA512'].get(),
+            }
 
         # Search a match with given hash
         if self.options['hash'].get():
             for encoding, hash in hashes.items():
                 if hash == self.options['hash'].get():
-                    print(True)
-                    print(encoding)
                     self.title(string = "Hashes - Match Found! Your %s hash matches" % encoding)
                     break
                 else:
